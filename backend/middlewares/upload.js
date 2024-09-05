d18ee6310a178f9c5ca7128c7694de4a775e94dd
Massamba505 <sabelomaphalala131@gmail.com>
2024-09-05 23:32:40 +0200

    uploads

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // File name
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only images are allowed.'));
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter
+});
+
+module.exports = upload;
