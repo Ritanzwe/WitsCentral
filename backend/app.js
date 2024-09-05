c01def520207802ef8c2a4f7612017b09acf9d34
Massamba505 <sabelomaphalala131@gmail.com>
2024-09-05 23:29:47 +0200

    uploads

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -14,9 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use("/uploads", express.static('uploads'));
 
 app.use('/api/', indexRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/events', eventsRouter);
 
 module.exports = app;
