03e0545a55d9aeab4b695fc5ea3791e3e6d6d5a8
Massamba505 <sabelomaphalala131@gmail.com>
2024-09-06 06:54:33 +0200

    app

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,55 +1,54 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login  from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
-import Home from './pages/Home'
-import {Toaster} from "react-hot-toast";
-import {useAuthContext} from "./context/AuthContext.jsx";
-import AddService from './components/AddService.jsx'
-import EventDashboard from './pages/EventDashBoard.jsx'
-import AdminEventPage from './pages/AdminDashboard.jsx'
-import AddCategories from './components/AddCategory.jsx'
-import Books from './pages/Books.jsx'
-import EventDetails from './pages/EventsDetails.jsx'
-import StudentDashboard from './components/studentDashboard.jsx'
-import CreateTutorProfile from './components/TutorProfileCreation.jsx'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import Home from './pages/Home';
+import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext.jsx';
+import AddService from './components/AddService.jsx';
+import EventDashboard from './pages/EventDashBoard.jsx';
+import AdminDashboard from './pages/AdminDashboard2.jsx'; // Import the AdminDashboard component
+import Events from './pages/Events.jsx'; // Import the AdminDashboard component
+import AddCategories from './components/AddCategory.jsx';
+import EventDetails from './pages/EventsDetails.jsx';
+import StudentDashboard from './components/studentDashboard.jsx';
+import CreateTutorProfile from './components/TutorProfileCreation.jsx';
+import ShopDashboard from './pages/ShopDashboard.jsx';
+import CreateShop from './components/CreateShop.jsx';
+import ShopDetails from './components/ShopDetails.jsx';
+import CreateProduct from './components/CreateProduct.jsx';
+import Forum from './pages/Forum.jsx';
 
 function App() {
-  const {authUser} = useAuthContext();
+  const { authUser } = useAuthContext();
   return (
     <div className='App'>
       <Routes>
-        <Route path = "/" element = {authUser? <Home/> : <Navigate to = {"/login"}/>  }/>
-        <Route path = "/login" element = {authUser?  <Navigate to= '/'/> : <Login /> }/>
-        <Route path = "/signup" element = {authUser? <Navigate to= '/'/> : <Signup /> }/>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
 
-        {/* ======================================================================================================== */}
+        <Route path="/addservices" element={authUser ? <AddService /> : <Navigate to="/" />} />
+        <Route path="/addcategories" element={authUser ? <AddCategories /> : <Navigate to="/" />} />
+        <Route path="/events" element={authUser ? <EventDashboard /> : <Navigate to="/" />} />
+        <Route path="/event/:id" element={authUser ? <EventDetails /> : <Navigate to="/" />} />
+        <Route path="/admin" element={authUser ? <AdminDashboard /> : <Navigate to="/" />} />
+        <Route path="/event-create" element={authUser ? <Events /> : <Navigate to="/" />} />
+        <Route path="/student" element={authUser ? <StudentDashboard /> : <Navigate to="/" />} />
+        <Route path="/tutorprofile" element={authUser ? <CreateTutorProfile /> : <Navigate to="/" />} />
+        <Route path="/shopownerprofile" element={authUser ? <CreateShop /> : <Navigate to="/" />} />
+        <Route path="/shop/:id" element={authUser ? <ShopDetails /> : <Navigate to="/" />} />
+        <Route path="/shop/:id/product/new" element={authUser ? <CreateProduct /> : <Navigate to="/" />} />
 
-        <Route path = "/addservices" element = {authUser? <AddService /> : <Navigate to= '/'/> }/>
-        <Route path = "/addcategories" element = {authUser? <AddCategories /> : <Navigate to= '/'/> }/>
-        <Route path = "/events" element = {authUser? <EventDashboard /> : <Navigate to= '/'/> }/>
-        <Route path = "/event/:id" element = {authUser? <EventDetails/> : <Navigate to= '/'/> }/>
-        <Route path = "/admin" element = {authUser? <AdminEventPage/> : <Navigate to= '/'/> }/>
-        <Route path = "/student" element = {authUser? <StudentDashboard/> : <Navigate to= '/'/> }/>
-        <Route path = "/tutorprofile" element = {authUser? <CreateTutorProfile/> : <Navigate to= '/'/> }/>
-
-
-        
-
-        {/* ================================================================================== */}
-        <Route path = "/service/tutoring" element = {authUser? <StudentDashboard/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/student-market" element = {authUser? <AdminEventPage/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/books" element = {authUser? <Books/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/elctronics" element = {authUser? <AdminEventPage/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/events" element = {authUser? <Navigate to= '/events'/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/forum" element = {authUser? <AdminEventPage/> : <Navigate to= '/'/> }/>
-        <Route path = "/service/CCDU" element = {authUser? <AdminEventPage/> : <Navigate to= '/'/> }/>
-      
-
+        <Route path="/service/tutoring" element={authUser ? <StudentDashboard /> : <Navigate to="/" />} />
+        <Route path="/service/student-market" element={authUser ? <ShopDashboard /> : <Navigate to="/" />} />
+        <Route path="/service/events" element={authUser ? <Navigate to="/events" /> : <Navigate to="/" />} />
+        <Route path="/service/forum" element={authUser ? <Forum /> : <Navigate to="/" />} />
+        {/* <Route path="/service/CCDU" element={authUser ? <AdminEventPage /> : <Navigate to="/" />} /> */}
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
